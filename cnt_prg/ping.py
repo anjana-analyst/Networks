2a5e64956c957ea5238703a4eb72a1c4fefddbff
arcadian002 <65863923+arcadian002@users.noreply.github.com>
2022-06-23 12:22:37 +0530

    Update ping.py

--- cnt_prg/ping.py
+++ cnt_prg/ping.py
@@ -1,8 +1,8 @@
 import os
 ip=['127.0.0.1']
 for ip_s in ip:
-    response = os.popen(f"ping {ip}").read()
+    response = os.popen(f"ping {ip_s}").read()
     if "Received = 4"in response:
-        print(f"UP {ip} Ping Successfull")
+        print(f"UP {ip_s} Ping Successfull")
     else:
-        print(f"DOWN {ip} Ping Unsuccessful")
+        print(f"DOWN {ip_s} Ping Unsuccessful")
